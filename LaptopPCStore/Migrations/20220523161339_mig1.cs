6b6f5b4a3e5bf201a84fa04d1f7601bbd9fcad46
Abhaya Simha S P <91654358+AbhayaSimhaSP@users.noreply.github.com>
2022-05-23 23:18:58 +0530

    CREATE VIEW CHANGES & DROPDOWNS

---
+++ LaptopPCStore/Migrations/20220523161339_mig1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LaptopPCStore.Migrations
+{
+    public partial class mig1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "id",
+                table: "inventories",
+                newName: "inv_id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "inv_id",
+                table: "inventories",
+                newName: "id");
+        }
+    }
+}
