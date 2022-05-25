b58ab7439d70a09ca60bad628b956068e60564e2
Nid@Laptop <22031046+NidPlays@users.noreply.github.com>
2022-05-25 12:34:03 +0530

    Update LaptopStoreModel.cs

--- LaptopPCStore/Models/LaptopStoreModel.cs
+++ LaptopPCStore/Models/LaptopStoreModel.cs
@@ -8,91 +8,111 @@ using System.Threading.Tasks;
 
 namespace LaptopPCStore.Models
 {
-        //add-migration -Context LaptopStoreContext frist
-        //update-database -Context LaptopStoreContext
-   
-        public class Laptop                     //USE PROP to generate 
-        {
-            [Key]
-            public int lap_id { get; set; }
-            public string lap_name { get; set; }
-            public string lap_cat { get; set; }
-            public string lap_ram { get; set; }
-            public string lap_cpu { get; set; }
-            public string lap_gpu { get; set; }
-            public string lap_disp_size { get; set; }
-            public string lap_disp_res { get; set; }
-            public string lap_disp_rr { get; set; }
-            public string lap_battrey { get; set; }
-            public string lap_storage { get; set; }
+    //add-migration -Context LaptopStoreContext frist
+    //update-database -Context LaptopStoreContext
+
+    public class Laptop                     //USE PROP to generate 
+    {
+        [Key]
+        public int lap_id { get; set; }
+        [Display(Name = "Laptop Name")]
+        public string lap_name { get; set; }
+        [Display(Name = "Category")]
+        public string lap_cat { get; set; }
+        [Display(Name = "RAM")]
+        public string lap_ram { get; set; }
+        [Display(Name = "CPU")]
+        public string lap_cpu { get; set; }
+        [Display(Name = "GPU")]
+        public string lap_gpu { get; set; }
+        [Display(Name = "Size")]
+        public string lap_disp_size { get; set; }
+        [Display(Name = "Resolution")]
+        public string lap_disp_res { get; set; }
+        [Display(Name = "Refresh Rate")]
+        public string lap_disp_rr { get; set; }
+        [Display(Name = "Battery")]
+        public string lap_battrey { get; set; }
+        [Display(Name = "Storage")]
+        public string lap_storage { get; set; }
             //public int lap_quantity { get; set; }
-        }
+    }
 
-        public class Vendor
-        {
-            [Key]
-            public int ven_id { get; set; }
-            public string ven_name { get; set; }
-            public string ven_number { get; set; }
-            public string ven_mail { get; set; }
-            public string ven_address { get; set; }      
+    public class Vendor
+    {
+        [Key]
+        public int ven_id { get; set; }
+        [Display(Name = "Vendor Name")]
+        public string ven_name { get; set; }
+        [Display(Name = "Phone Number")]
+        public string ven_number { get; set; }
+        [Display(Name = "Email")]
+        public string ven_mail { get; set; }
+        [Display(Name = "Address")]
+        public string ven_address { get; set; }      
 
-        }
+    }
 
-        public class Purchase
-        {
-            [Key]
-            public int purchase_id { get; set; }
+    public class Purchase
+    {
+        [Key]
+        public int purchase_id { get; set; }
 
-            [ForeignKey("fk2")]
-            [Display(Name = "Vendor Name")]
-            public int? ven_id { get; set; }
-            public Vendor fk2 { get; set; }
+        [ForeignKey("fk2")]
+        [Display(Name = "Vendor Name")]
+        public int? ven_id { get; set; }
+        public Vendor fk2 { get; set; }
 
-            [ForeignKey("fk3")]
-            [Display(Name = "Laptop Name")]
-            public int? lap_id { get; set; }
-            public Laptop fk3 { get; set; }
-
-            public int purchase_quantity { get; set; }
-            public DateTime purchase_date { get; set; }
-            public int purchase_cost { get; set; }
-        }
-
-        
-        public class Invoice
-        {
-            [Key]
-            public int invoice_id { get; set; }
-            public string userID { get; set; } //httpcontext.current.user.identity.name
-
-            [ForeignKey("fk6")]
-            [Display(Name = "Laptop Name")]
-            public int lap_id { get; set; }
-            public Laptop fk6 { get; set; }
-
-            public int quantity { get; set; }
-
-            public string inv_name { get; set; }
-            public string inv_phone { get; set; }
-            public string inv_mail { get; set; }
-            public string inv_address { get; set; }
-        }
+        [ForeignKey("fk3")]
+        [Display(Name = "Laptop Name")]
+        public int? lap_id { get; set; }
+        public Laptop fk3 { get; set; }
+        [Display(Name = "Quantity")]
+        public int purchase_quantity { get; set; }
+        [Display(Name = "Date")]
+        public DateTime purchase_date { get; set; }
+        [Display(Name = "Cost")]
+        public int purchase_cost { get; set; }
+    }
 
 
-        public class Inventory
-        {
-            //[Key]
-            //public int inv_id { get; set; }
-            [Key]
-            [ForeignKey("fk4")]
-            [Display(Name = "Laptop Name")]
-            public int lap_id { get; set; }
-            public Laptop fk4 { get; set; }
+    public class Invoice
+    {
+        [Key]
+        public int invoice_id { get; set; }
+        public string userID { get; set; } //httpcontext.current.user.identity.name
 
-            public int quantity { get; set; }
+        [ForeignKey("fk6")]
+        [Display(Name = "Laptop Name")]
+        public int lap_id { get; set; }
+        public Laptop fk6 { get; set; }
+        [Display(Name = "Quantity")]
+        public int quantity { get; set; }
+        [Display(Name = "Name")]
+        public string inv_name { get; set; }
+        [Display(Name = "Phone")]
+        public string inv_phone { get; set; }
+        [Display(Name = "Email")]
+        public string inv_mail { get; set; }
+        [Display(Name = "Address")]
+        public string inv_address { get; set; }
+    }
 
-        }
+
+    public class Inventory
+    {
+        //[Key]
+        //public int inv_id { get; set; }
+        [Key]
+        [ForeignKey("fk4")]
+        [Display(Name = "Laptop Name")]
+        public int lap_id { get; set; }
+        public Laptop fk4 { get; set; }
+        [Display(Name = "Quantity")]
+
+        public int quantity { get; set; }
+
+    }
 
     public class LaptopStoreContext : DbContext
         {
