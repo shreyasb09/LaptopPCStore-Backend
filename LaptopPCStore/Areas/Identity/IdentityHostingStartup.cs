a625a76d14f7ff42d9e6b8e0ed8a86729f6e1f07
Nid@Laptop <22031046+NidPlays@users.noreply.github.com>
2022-05-25 10:51:02 +0530

    Added Custom Login

---
+++ LaptopPCStore/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using LaptopPCStore.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(LaptopPCStore.Areas.Identity.IdentityHostingStartup))]
+namespace LaptopPCStore.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
